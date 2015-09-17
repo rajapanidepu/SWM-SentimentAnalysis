a6d382d89d2ee2e5877271afbbd62e975626c6e7
RaviTejas3 <ravitejas3@gmail.com>
2015-09-16 17:31:03 -0700

    added appleIndex.py to scrape all apple iphones

---
+++ appleIndex.py
@@ -0,0 +1,33 @@
+#http://www.amazon.com/s/ref=sr_pg_15?fst=as%3Aoff&rh=n%3A2335752011%2Cn%3A7072561011%2Ck%3Aiphone%2Cp_89%3AApple&page=15&keywords=iphone&ie=UTF8&qid=1442374149
+
+
+from pprint import pprint
+import BeautifulSoup
+import requests
+import re
+import urllib
+
+
+url = "http://www.amazon.com/s/ref=sr_pg_1?fst=as%3Aoff&rh=n%3A2335752011%2Cn%3A7072561011%2Ck%3Aiphone%2Cp_89%3AApple&keywords=iphone&ie=UTF8&qid=1442374599"
+print url
+response = requests.get(url)
+
+soup = BeautifulSoup.BeautifulSoup(response.content)
+curPage = int(soup.find('span',{'class':'pagnCur'}).string)
+totalPages = int(soup.find('span',{'class':'pagnDisabled'}).string)
+print curPage,'/',totalPages
+# while curPage <= totalPages:
+currentPage = soup.find('span',{'class':'pagnCur'})
+print currentPage
+while curPage <= totalPages:
+	results = soup.find(id='s-results-list-atf')
+	# for li in results:
+	# 	print li['id']
+	print len(results.findAll('li')),' - ' ,soup.find('span',{'class':'pagnDisabled'}).string
+	nextPage = currentPage.findNext('span')
+	nextPageURL =  nextPage.a['href']
+	response = requests.get(url)
+	soup = BeautifulSoup.BeautifulSoup(response.content)
+	curPage+=1
+	
+
---
+++ readme.txt
@@ -0,0 +1,2 @@
+to connect to aws
+ssh -i oceanblues.pem ubuntu@ec2-52-88-9-177.us-west-2.compute.amazonaws.com
