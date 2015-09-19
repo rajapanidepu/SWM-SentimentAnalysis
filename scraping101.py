37aa5c70c79a36c527d7258f34fa1cde4277ec15
kranthi <kranthisai.d@gmail.com>
2015-09-19 11:28:34 -0700

    mysmalledit

--- scraping101.py
+++ scraping101.py
@@ -4,7 +4,7 @@ import requests
 import re
 import urllib
 
-
+print "Kranthi"
 #product url
 url = 'http://www.amazon.com/dp/B0074R0Z3O'
 response = requests.get(url, headers={'User-agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36'})
